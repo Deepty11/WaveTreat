aeb74e58e331301148aac681c6d6c42ce8408ab5
Rehnuma Reza <rehnuma.deepty@therapservices.net>
2022-12-29 16:10:40 +0600

    Static wave

--- WaveTreat/ViewController.swift
+++ WaveTreat/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let waveView = WaveView(frame: CGRect(x: 0, y: view.frame.height / 2, width: view.frame.width, height: view.frame.height / 2))
+        view.addSubview(waveView)
+        
+        
+        
+        
     }
 
 
---
+++ WaveTreat/Views/WaveView.swift
@@ -0,0 +1,93 @@
+//
+//  WaveView.swift
+//  WaveTreat
+//
+//  Created by Rehnuma Reza on 29/12/22.
+//
+
+import Foundation
+import UIKit
+
+class WaveView: UIView{
+    enum Direction{
+        case right
+        case left
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        createLabel()
+    }
+    
+    private weak var displayLink: CADisplayLink?
+    private var startTime: CFTimeInterval = 0
+    
+    var myLayer = CAShapeLayer()
+    
+    var speed:Double = 10 // to animate the wave
+    var frequency = 55.0 // to change the height
+    var parameterA = 1.5 // paramA and paramB are to change the length of the wave
+    var parameterB = 9.0
+    var phase = 0.0
+    
+    var preferredColor = UIColor.systemCyan
+    
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath()
+        myLayer.frame = rect
+
+        let width = Double(self.frame.width)
+        let height = Double(self.frame.height)
+
+        let mid = height * 0.5
+
+        let waveLength = width / frequency
+        let waveHeightCoef = Double(frequency)
+
+        path.move(to: CGPoint(x: 0, y: frame.maxY))
+        path.addLine(to: CGPoint(x: 0, y: mid))
+
+        for x in stride(from: 0, to: width, by: 1){
+            let actualX = x / waveLength
+            let sine = -cos(parameterA * (actualX + phase)) * sin((actualX + phase)/parameterB)
+
+            let y = waveHeightCoef * sine + mid
+
+            path.addLine(to: CGPoint(x: x, y: y))
+
+        }
+
+        path.addLine(to: CGPoint(x: CGFloat(width), y: frame.maxY))
+        path.addLine(to: CGPoint(x: 0, y: frame.maxY))
+
+        myLayer.path = path.cgPath
+        myLayer.fillColor = preferredColor.cgColor
+        myLayer.strokeColor = preferredColor.cgColor
+        self.layer.addSublayer(myLayer)
+
+    }
+    
+    func createLabel(){
+        let label = UILabel(frame: CGRect(x: 10, y: 10, width: self.frame.width - 10, height: 20))
+        label.font = .boldSystemFont(ofSize: 18.0)
+        label.textColor = .orange
+        label.text = "What a wave!!"
+        label.textAlignment = .center
+        
+        addSubview(label)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0),
+            label.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0),
+            label.topAnchor.constraint(equalTo: self.topAnchor, constant: 300),
+            label.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 100)
+        ])
+        
+        
+    }
+
+}
